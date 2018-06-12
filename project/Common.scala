b6f872d96a48e1dbfe04dce141ece49b0955432d
KASHIMA Kazuo <k4200@kazu.tv>
2018-06-12 12:38:52 +0900

    Bump to 3.3.0-SNAPSHOT

--- project/Common.scala
+++ project/Common.scala
@@ -1,7 +1,7 @@
 
 
 object Common {
-  def version = "3.2.5"
+  def version = "3.3.0-SNAPSHOT"
   def playVersion = System.getProperty("play.version", "2.6.13")
   def scalaVersion = System.getProperty("scala.version", "2.12.4")
   def crossScalaVersions = Seq(scalaVersion, "2.11.12")
