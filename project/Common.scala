e051f83f47db39515a431354f4ef5c316f9dd7b4
KASHIMA Kazuo <k4200@kazu.tv>
2018-06-12 12:33:06 +0900

    Bump Play version to 2.6.13

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@
 
 object Common {
   def version = "3.2.5"
-  def playVersion = System.getProperty("play.version", "2.6.12")
+  def playVersion = System.getProperty("play.version", "2.6.13")
   def scalaVersion = System.getProperty("scala.version", "2.12.4")
   def crossScalaVersions = Seq(scalaVersion, "2.11.12")
 }
